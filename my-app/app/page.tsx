238af101662d1775f24a6e51049c68beb597974c
unknown <goluchoudhary7808@gmail.com>
2023-11-10 12:51:10 +0530

    remove bgc

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,1 +1,79 @@
+"use client"
 
+import React, { useState } from 'react';
+
+const Home = () => {
+  const [board, setBoard] = useState(Array(9).fill(null)); // Represents the tic-tac-toe board
+  const [xIsNext, setXIsNext] = useState(true); // Indicates if it's X's turn
+
+  const handleClick = (index) => {
+    const newBoard = [...board];
+    // If the square is already filled or game is won, do nothing
+    if (newBoard[index] || calculateWinner(newBoard)) {
+      return;
+    }
+    newBoard[index] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(board);
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
